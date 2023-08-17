63371b43ad5e5337e1a3720216aeaf9b9434dd33
Udaykolhe <udaykolhe03@gmail.com>
2023-08-17 10:50:10 +0530

    Validation_Done

--- src/app/model/student.ts
+++ src/app/model/student.ts
@@ -3,9 +3,13 @@ export class Student {
      *
      */
     constructor(
-        public name: string,
-        public age: number,
-        public email: string
+        public name?: string,
+        public age?: number,
+        public email?: string,
+        public gender? : string,
+        public country? : string,
+        public zip? : number,
+        public accept? : boolean
     ) { }
 
 }
--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -7,10 +7,30 @@ import { Student } from '../model/student';
   styleUrls: ['./template-driven-form.component.css']
 })
 export class TemplateDrivenFormComponent {
- std = new Student("Vipul",23,"uday@gmail.com");
+  SelectedHobbies : string[]=[];
+ std = new Student();
+ constructor(){
+  this.std.country="";
+ }
   save(formData:any){
     // console.log(formData);
     const std = new Student(formData.name,formData.age,formData.email);
-    console.log(std);
+    console.log(this.std);
+    console.log(this.SelectedHobbies);
+    
+  }
+
+ 
+  onChange(e:any){
+    let selectedValue = e.target.value;
+    let checked = e.target.checked;
+    if (checked) {
+      this.SelectedHobbies.push(selectedValue);
+    }else{
+      let index = this.SelectedHobbies.indexOf(selectedValue);
+      this.SelectedHobbies.splice(index, 1);
+    }
+  //   console.log(selectedValue,checked)
+     console.log(this.SelectedHobbies)
   }
 }
